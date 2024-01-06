4bfaf33578637ba89749b2489e99f7f02c3106ad
s0ders <s0ders@pm.me>
2024-01-06 17:19:29 +0100

    fix(mappings.lua): new mapping for Telescope and Git

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+-- Disable default
+M.disabled = {
+  n = {
+      ["<leader>gt"] = "",
+  }
+}
+
 -- Custom mappings to change indent with <Tab> instead of < and >
 M.abc = {
   v = {
@@ -7,6 +14,16 @@ M.abc = {
      ["<S-Tab>"] = {"<", "Decrease indent"}
   },
 
+  n = {
+    -- Windows management
+    ["<leader>ww"] = {"<C-w>v", "Split window vertically"},
+    ["<leader>wq"] = {"<C-w>q", "Quit current windows"},
+
+    -- Telescope Git related
+    ["<leader>gc"] = {":Telescope git_commits <CR>", "Git commits"},
+    ["<leader>gs"] = {":Telescope git_status <CR>", "Git status"},
+    ["<leader>gr"] = {":Telescope git_branches <CR>", "Git branches"},
+  },
 }
 
 return M
