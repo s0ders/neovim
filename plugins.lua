524636aa6c588f3aa5a9d72eeaeda1be028178e5
s0ders <s0ders@pm.me>
2024-01-07 13:41:08 +0100

    feat(plugins): added TODOs plugin and symbol outlines with VSCode icons

--- plugins.lua
+++ plugins.lua
@@ -27,7 +27,7 @@ local plugins = {
     end,
   },
   {
-  "ray-x/go.nvim",
+    "ray-x/go.nvim",
     dependencies = {  -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
@@ -41,28 +41,71 @@ local plugins = {
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
   {
-      "nvim-treesitter/nvim-treesitter",
-      opts = {
-        ensure_installed = {
-          -- defaults 
-          "vim",
-          "lua",
-          "bash",
-          -- dev 
-          "html",
-          "css",
-          "javascript",
-          "go",
-          "gomod",
-          "gosum",
-          "json",
-          "yaml",
-          "sql",
-          "markdown",
-          "dockerfile",
-        },
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+        "bash",
+        -- dev 
+        "html",
+        "css",
+        "javascript",
+        "go",
+        "gomod",
+        "gosum",
+        "json",
+        "yaml",
+        "sql",
+        "markdown",
+        "dockerfile",
       },
     },
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    cmd = "SymbolsOutline",
+    opts = {
+      symbols = {
+        File = { icon = "", hl = "@text.uri" },
+        Module = { icon = "󰕳", hl = "@namespace" },
+        Namespace = { icon = "", hl = "@namespace" },
+        Package = { icon = "", hl = "@namespace" },
+        Class = { icon = "", hl = "@type" },
+        Method = { icon = "", hl = "@method" },
+        Property = { icon = "", hl = "@method" },
+        Field = { icon = "", hl = "@field" },
+        Constructor = { icon = "", hl = "@constructor" },
+        Enum = { icon = "", hl = "@type" },
+        Interface = { icon = "", hl = "@type" },
+        Function = { icon = "󰊕", hl = "@function" },
+        Variable = { icon = "󰀫", hl = "@constant" },
+        Constant = { icon = "󰏿", hl = "@constant" },
+        String = { icon = "", hl = "@string" },
+        Number = { icon = "#", hl = "@number" },
+        Boolean = { icon = "", hl = "@boolean" },
+        Array = { icon = "", hl = "@constant" },
+        Object = { icon = "⦿", hl = "@type" },
+        Key = { icon = "", hl = "@type" },
+        Null = { icon = "NULL", hl = "@type" },
+        EnumMember = { icon = "", hl = "@field" },
+        Struct = { icon = "", hl = "@type" },
+        Event = { icon = "🗲", hl = "@type" },
+        Operator = { icon = "+", hl = "@operator" },
+        TypeParameter = { icon = "𝙏", hl = "@parameter" },
+        Component = { icon = "", hl = "@function" },
+        Fragment = { icon = "", hl = "@constant" },
+      }
+    }
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    opts = {}
+  }
 }
 
 return plugins
